5a59cd1d636eb5723f5fda8b9ee60038a3658299
Michal Musial <michal.musial554@gmail.com>
2021-01-05 15:23:46 +0100

    initial

--- server.js
+++ server.js
@@ -13,4 +13,10 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 // Rest of server.js code below
