76d1f93b509c91ffcaaf35b34506633041e91e48
Michal Musial <michal.musial554@gmail.com>
2021-01-18 16:00:05 +0100

    cart

--- src/app/screens/cart-view/cart-view.component.ts
+++ src/app/screens/cart-view/cart-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {CartService} from '../../services/cart/cart.service';
 import {CartItem} from '../../model/cart-item';
 import {formatCurrency} from '../../model/formatCurrency';
@@ -15,7 +15,7 @@ export class CartViewComponent implements OnInit {
   public shoppingCartItems: CartItem[] = [];
   displayedColumns: string[] = ['title', 'quantity', 'totalPrice', 'delete'];
   public formatCurrency: any = formatCurrency;
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService,  private changeDetectorRefs: ChangeDetectorRef) {
   }
   ngOnInit() {
     this.cartItems = this.cartService.getItems();
@@ -25,24 +25,22 @@ export class CartViewComponent implements OnInit {
     });
 
   }
-  addOneMoreProduct(element: any) {
-    this.cartService.addToCart(element.product, 1);
-  }
-
-  removeOneProduct(element: any) {
-
-  }
   removeProductFromCart(element: CartItem) {
     this.cartService.removeFromCart(element);
+    this.changeDetectorRefs.detectChanges();
   }
   // Increase Product Quantity
   public increment(product: any, quantity: number = 1) {
+    console.log('i');
     this.cartService.updateCartQuantity(product, quantity);
+    this.changeDetectorRefs.detectChanges();
   }
 
   // Decrease Product Quantity
   public decrement(product: any, quantity: number = -1) {
+    console.log('d');
     this.cartService.updateCartQuantity(product, quantity);
+    this.changeDetectorRefs.detectChanges();
   }
 
 }
