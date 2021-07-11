b30847db07ac8c618e26180114ccd0d0aced00a4
Michal Musial <michal.musial554@gmail.com>
2021-07-11 21:15:24 +0200

    fix

---
+++ src/app/components/wrong-password-dialog/wrong-password-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wrong-password-dialog',
+  templateUrl: './wrong-password-dialog.component.html',
+  styleUrls: ['./wrong-password-dialog.component.styl']
+})
+export class WrongPasswordDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
